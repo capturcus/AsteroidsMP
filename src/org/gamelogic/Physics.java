38c97c658e436f1c7d8015f5a562c42a0cd081ff
Klaudia Grygoruk <kg346915@students.mimuw.edu.pl>
2015-04-26 00:41:31 +0200

    New package: org.gamelogic

---
+++ src/org/gamelogic/Physics.java
@@ -0,0 +1,11 @@
+package org.gamelogic;
+
+import math.geom2d.Vector2D;
+
+public class Physics {
+	
+	public Vector2D getMove(int mass, Vector2D velocity, int timeInterval) {
+		return new Vector2D(10, 0);
+	}
+	
+}
