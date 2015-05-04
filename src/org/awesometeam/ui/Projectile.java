bfe7d17eccd511c803b532e6df15d1be43134ea9
capturcus@flondra <consommepanchi@gmail.com>
2015-05-04 18:01:50 +0200

    ui

--- src/org/awesometeam/ui/Projectile.java
+++ src/org/awesometeam/ui/Projectile.java
@@ -5,10 +5,33 @@
  */
 package org.awesometeam.ui;
 
+import math.geom2d.Point2D;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
 /**
  *
  * @author Marcin
  */
-public class Projectile {
-    
+public class Projectile extends Actor {
+
+    public Projectile(Point2D pos, double angle, Image img) {
+        super(pos, angle, img);
+    }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
+    }
+
 }
