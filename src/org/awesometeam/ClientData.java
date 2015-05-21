acba7d7694c0c0fd8acc283f6bdffd72cb8785fd
Michal Cylwik <m.cylwik@students.mimuw.edu.pl>
2015-05-21 22:00:34 +0200

    Added base server classes.

---
+++ src/org/awesometeam/ClientData.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.awesometeam;
+
+import java.net.InetAddress;
+
+/**
+ *
+ * @author michal
+ */
+class ClientData {
+    public InetAddress address;
+    public int port;
+    public int ID;
+}
