2a0baecc1144c33113c39c96c54c60dbc21ace1d
Michal Cylwik <m.cylwik@students.mimuw.edu.pl>
2015-05-25 16:43:06 +0200

    Server v1.3: Added fully operationa ServerSentData.

--- src/org/awesometeam/servernetworking/Asteroid.java
+++ src/org/awesometeam/servernetworking/Asteroid.java
@@ -5,12 +5,29 @@
  */
 package org.awesometeam.servernetworking;
 
+import math.geom2d.Point2D;
+
 /**
  *
  * @author michal
  */
 public class Asteroid {
-    public double x;
-    public double y;
+    Point2D pos;
     public int size;
+    
+    public Asteroid() {
+        pos = new Point2D(0, 0);
+        size = 0;
+    }
+    
+    public Asteroid(Point2D p, int s) {
+        pos = new Point2D(p);
+        size = s;
+    }
+    
+    public Asteroid(org.awesometeam.gamelogic.Asteroid as)
+    {
+        pos = as.getPosition();
+        size = as.getSize();
+    }
 }
