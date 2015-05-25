42771df551311fe6b95321fc268a6c1ec61588ca
marcin@flondra <consommepanchi@gmail.com>
2015-05-25 13:57:36 +0200

    minor fixes

--- src/org/awesometeam/AsteroidServerMain.java
+++ src/org/awesometeam/AsteroidServerMain.java
@@ -2,9 +2,9 @@ package org.awesometeam;
 
 public class AsteroidServerMain {
 
-	public static void main(String[] args) {
-		AsteroidServerReceiver asr = new AsteroidServerReceiver();
-		asr.startThread();
-	}
+    public static void main(String[] args) {
+        AsteroidServerReceiver asr = new AsteroidServerReceiver();
+        asr.startThread();
+    }
 
 }
