a86ec1298fc9e652e3ab25b5d32f130f2cb77ecf
Klaudia Grygoruk <kg346915@students.mimuw.edu.pl>
2015-06-08 23:31:17 +0200

    usunięcie zbędnego wyjątku

--- src/org/awesometeam/gamelogic/GameUpdater.java
+++ src/org/awesometeam/gamelogic/GameUpdater.java
@@ -23,33 +23,23 @@ public class GameUpdater extends TimerTask {
     public void run() {
 		//System.out.println("update");
         // TODO Auto-generated method stub
-    //    KeyPresses keyPresses = new KeyPresses();
-    //    boolean[] Presses = new boolean[2];//GameState.getKeyPresses();
-    //    keyPresses.setKeyPresses(Presses);
-        
+        //    KeyPresses keyPresses = new KeyPresses();
+        //    boolean[] Presses = new boolean[2];//GameState.getKeyPresses();
+        //    keyPresses.setKeyPresses(Presses);
+
         //ArrayList<KeyPresses> playersKeyPresses = new ArrayList<KeyPresses>();
         TreeMap<Integer, KeyPresses> playersKeyPresses = SharedMemoryServerReceived.getInstance().getData().map;
         //System.out.println("GameLogic                  Debug" + playersKeyPresses);
         //playersKeyPresses.put(0, keyPresses);
-        
+
         //playersKeyPresses.add(keyPresses);
-        try {
-            double time = timeInterval;
-            time /= 1000;
-            actorManager.update(playersKeyPresses, time);
-            SharedMemoryServerSent.getInstance().writeData(
-                    actorManager.getActorLists().getSpaceshipList(),
-                    actorManager.getActorLists().getProjectileList(),
-                    actorManager.getActorLists().getAsteroidList());
-            //System.out.println("GameLogic                  Debug");
-        } catch (IncorrectListLengthAsteroidsMPGLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-//		System.out.println(actorManager.getActorList().toString());
-//		System.out.println(actorManager.getAsteroidList().toString());
-//		System.out.println(actorManager.getSpaceshipList().toString());
-//		System.out.println(actorManager.getProjectileList().toString());
+        double time = timeInterval;
+        time /= 1000;
+        actorManager.update(playersKeyPresses, time);
+        SharedMemoryServerSent.getInstance().writeData(
+                actorManager.getActorLists().getSpaceshipList(),
+                actorManager.getActorLists().getProjectileList(),
+                actorManager.getActorLists().getAsteroidList());
     }
 
     public void setTimeInterval(int interval) {
